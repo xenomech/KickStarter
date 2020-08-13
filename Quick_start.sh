d5746f67f2079d32a37cf7098778a08127d1dfbd
xenomech <gokulsmenon227@gmail.com>
2020-08-13 08:04:28 +0530

    ✅  added updates and test!

--- Quick_start.sh
+++ Quick_start.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
 mkdir $1
 cd $1
+
+echo "select framework F : flutter , R : react"
+read FRAMEWORK
 mkdir "Frontend"
 cd "Frontend"
-flutter create $1
+case $FRAMEWORK in
+	f)
+		echo "initializing flutter app"
+        flutter create $1
+        break
+		;;
+	r)
+		echo "initializing react app"
+        npx create-react-app $1
+		break
+		;;
+	*)
+		echo "Sorry, I don't understand"
+		;;
+esac
 cd ..
 mkdir "Backend"
 cd "Backend"
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # The kick starter script
+
+> chmod +X Quick_start.sh
+
+> ./Quick_start.sh
+
+Sample output
+
+---
+
+./Quick_start.sh mynewapp
+
+select framework F : flutter , R : react
+
+f
+
+initializing flutter app
+
+Creating project mynewapp...
+
+mynewapp/.metadata (created)
+
+---
